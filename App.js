1bb885965b86b6a91c73963bf2733a6c9e5016da
stevenjlance <steven.j.lance@gmail.com>
2022-10-19 13:04:50 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+
+    return (
+        <div>
+            <p>You clicked 1 times</p>
+            <button>
+                Click me!
+            </button>
+        </div>
+    )
+}
+
+export default App;

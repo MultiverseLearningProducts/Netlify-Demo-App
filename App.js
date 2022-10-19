f5daa649e18fa6c796c641794d0f962e3f818257
stevenjlance <steven.j.lance@gmail.com>
2022-10-19 13:10:01 -0400

    testing out auto deplys

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App(){
-
+    const [count, setCount] = useState(0);
     return (
         <div>
-            <p>You clicked 1 times</p>
-            <button>
+            <p>You clicked {count} times</p>
+            <button onClick = {() => setCount(count+1)}>
                 Click me!
             </button>
         </div>
